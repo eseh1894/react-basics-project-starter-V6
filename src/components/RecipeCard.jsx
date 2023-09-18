3a37d407bd238577330ca4435eb18f1ff327728b
eseh1894 <esehosagie1894@gmail.com>
2023-09-18 14:25:10 +0200

    Commit

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,19 @@
+function RecipeCard({ recipe }) {
+  return (
+    <li>
+      <h2>{recipe.label}</h2>
+      <img src={recipe.image} alt={recipe.label} />
+      {recipe.dietLabels.length > 0 && (
+        <p>Diet Label: {recipe.dietLabels.join(", ")}</p>
+      )}
+      {recipe.cautions.length > 0 && (
+        <p>Cautions: {recipe.cautions.join(", ")}</p>
+      )}
+      <p>Meal Type: {recipe.mealType.join(", ")}</p>
+      <p>Dish Type: {recipe.dishType.join(", ")}</p>
+      <p>Health Labels: {recipe.healthLabels.join(", ")}</p>
+    </li>
+  );
+}
+
+export default RecipeCard;
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import { data } from "../utils/data";
+import RecipeCard from "../components/RecipeCard";
 
 function RecipeListPage() {
   return (
     <div>
       <h1>Recipe List</h1>
       <ul>
-        {data.hits.map((hit, index) => {
-          const recipe = hit.recipe;
-          return (
-            <li key={index}>
-              <h2>{recipe.label}</h2>
-              <img src={recipe.image} alt={recipe.label} />
-              {recipe.dietLabels.length > 0 && (
-                <p>Diet Label: {recipe.dietLabels.join(", ")}</p>
-              )}
-              {recipe.cautions.length > 0 && (
-                <p>Cautions: {recipe.cautions.join(", ")}</p>
-              )}
-              <p>Meal Type: {recipe.mealType.join(", ")}</p>
-              <p>Dish Type: {recipe.dishType.join(", ")}</p>
-              <p>Health Labels: {recipe.healthLabels.join(", ")}</p>
-            </li>
-          );
-        })}
+        {data.hits.map((hit, index) => (
+          <RecipeCard key={index} recipe={hit.recipe} />
+        ))}
       </ul>
     </div>
   );
