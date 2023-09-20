24c783572b1df7980fa68677d16323bb245ba5e1
eseh1894 <esehosagie1894@gmail.com>
2023-09-20 16:44:33 +0200

    commit

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,42 +1,47 @@
-import { Box, Text, Image, Badge, Flex } from "@chakra-ui/react";
+import { Box, Text, Image, Badge, Flex, SimpleGrid } from "@chakra-ui/react";
 
 export function RecipeCard({ recipe }) {
   return (
     <>
-      <Flex flexWrap="wrap" justify="space-between">
-        <Box
-          spacing={10}
-          maxW="sm"
-          marginBottom="20px"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          boxShadow="md"
-          cursor="pointer"
-        >
-          <Image src={recipe.image} alt={recipe.label} />
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        <Flex flexWrap="wrap" justify="space-between">
+          <Box
+            spacing={10}
+            maxW="sm"
+            marginBottom="20px"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            boxShadow="md"
+            cursor="pointer"
+          >
+            <Image src={recipe.image} alt={recipe.label} />
 
-          <Box p="6">
-            <Text fontWeight="semibold" fontSize="lg" mb="2">
-              {recipe.label}
-            </Text>
-            {recipe.dietLabels.length > 0 && (
-              <Badge colorScheme="green" mb="2">
-                {recipe.dietLabels[0]}
-              </Badge>
-            )}
-            <Text fontSize="sm" color="gray.500">
-              Meal Type: {recipe.mealType.join(", ")}
-            </Text>
-            <Text fontSize="sm" color="gray.500">
-              Dish Type: {recipe.dishType.join(", ")}
-            </Text>
-            <Text fontSize="sm" color="gray.500" mt="2">
-              Health Labels: {recipe.healthLabels.join(", ")}
-            </Text>
+            <Box p="6">
+              <Text fontWeight="semibold" fontSize="lg" mb="2">
+                {recipe.label}
+              </Text>
+              {recipe.dietLabels.length > 0 && (
+                <Badge colorScheme="green" mb="2">
+                  {recipe.dietLabels[0]}
+                </Badge>
+              )}
+              <Text fontSize="sm" color="gray.500">
+                Meal Type: {recipe.mealType.join(", ")}
+              </Text>
+              <Text fontSize="sm" color="gray.500">
+                Dish Type: {recipe.dishType.join(", ")}
+              </Text>
+              <Text fontSize="sm" color="gray.500" mt="2">
+                Health Labels: {recipe.healthLabels.join(", ")}
+              </Text>
+            </Box>
           </Box>
-        </Box>
-      </Flex>
+        </Flex>
+      </SimpleGrid>
     </>
   );
 }
